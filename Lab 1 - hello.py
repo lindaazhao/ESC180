4ecc9e0e0a6ed720a1fad05415bf3b9a356f5048
Linda Zhao <linda.zhao23@gmail.com>
2021-09-14 17:13:17 -0400

    initialized repo, completed Lab 1

---
+++ Lab 1 - hello.py
@@ -0,0 +1,26 @@
+# Problem 1
+print("Hello, Python")
+
+# Problem 2
+print("Hello, Claire")
+print("Hello, Linda")
+
+# Problem 3
+name1 = "Claire"
+name2 = "Linda"
+
+print("Hello, " + name1 + " and " + name2 + ". Your names are " + name1 + " and " +
+      name2 + ". Hi there. Your names are still " + name1 + " and " + name2 + ".")
+
+# Problem 4
+name1 = "Prof. Cluett"
+name2 = "Prof. Thywissen"
+# Visual confirmation that variable values changed
+# print("Hello, " + name1 + " and " + name2)
+
+# Problem 5
+greetee = str(input("What is your name?: "))
+if greetee == "Lord Voldemort":
+    print("I'm not talking to you.")
+else:
+    print("Hello, " + greetee)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ESC180
  Labs & Projects for ESC180: Intro to Programming
+ By Claire Zhang & Linda Zhao
