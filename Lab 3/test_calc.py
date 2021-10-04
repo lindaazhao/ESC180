800e69556ce705ca7b0e90ccb364dde160024e75
Linda Zhao <linda.zhao23@gmail.com>
2021-10-04 11:46:56 -0400

    Updated Lab 3 w/feedback from TA

--- Lab 3/test_calc.py
+++ Lab 3/test_calc.py
@@ -1,8 +1,14 @@
 import lab02
+from lab02 import * # **(Maybe) don't do this :^), 
+# **but it allows you to use functions from lab02 without writing "lab02." before calling the function
 
 if __name__ == "__main__":
     lab02.initialize()
     lab02.add(23)
+
+    # **This will pop an error if it is false, so it's a good way to check things that should be true
+    assert lab02.get_current_value == 23, "Test 1 failed"
+
     if (lab02.get_current_value() == 23): #test if current_value = 23
         print("Test 1 passed!")
     else:
