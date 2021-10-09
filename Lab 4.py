7866140bc70d22f3625649e68aec7e0f6822d11a
Linda Zhao <linda.zhao23@gmail.com>
2021-10-09 19:23:04 -0400

    Finished Lab 4 w/TA feedback

--- Lab 4.py
+++ Lab 4.py
@@ -24,18 +24,25 @@ def list_to_str(lis):
     return string
     # There's probably a more efficient way to do this
 
+    # Could use a separator, string.join, 
+
 print("List to String Test:", list_to_str([1, 2, 3, 4, 5]))
+print(list_to_str)
 
 ## ==================================================
 ## Problem 3: Comparing Lists
 def lists_are_the_same(list1, list2):
-    for i in range(len(list1)):
-        if list1[i] != list2[i]:
-            return False
+    if len(list1) == len(list2):
+        for i in range(len(list1)):
+            if list1[i] != list2[i]:
+                return False
+    else:
+        return False
     return True
 
 print("Same Lists Test 1:", lists_are_the_same([4, 5, 6], [1, 2, 3]))
 print("Same Lists Test 2:", lists_are_the_same([1, 2, 3], [1, 2, 3]))
+print(lists_are_the_same([1, 2], [1, 2, 3]))
 
 ## ==================================================
 ## Problem 4: Simplifying Fractions
@@ -67,9 +74,32 @@ def approx_pi(n):
         res = ((-1)**num_terms)/(2*num_terms + 1)
         quarter_pi += res
         num_terms += 1
+        if num_terms % 1000000 == 0:
+            print("Trying my best")
 
     print(quarter_pi*4)
     return num_terms
 
 print(approx_pi(3))
 print(approx_pi(6))
+# print(approx_pi(10))
+
+## ==================================================
+## Problem 6: Euclid's GCD
+# Euclids algorithm: Given two numbers not prime to one another, to find their greatest common factor
+# If A = 0 then GCD(A,B)=B, since the GCD(0,B)=B, and we can stop.
+# If B = 0 then GCD(A,B)=A, since the GCD(A,0)=A, and we can stop.
+# Write A in quotient remainder form (A = B⋅Q + R)
+# Find GCD(B,R) using the Euclidean Algorithm since GCD(A,B) = GCD(B,R)
+
+def euclids_alg(a,b):
+    max_input = max(a, b)
+    min_input = min(a, b)
+    while min_input != 0:
+        r = max_input % min_input
+        max_input = min_input
+        min_input = r
+    print(max_input)
+    return(str(a//max_input)) + "/" + str(b//max_input)
+
+print(euclids_alg(1872,8493))
