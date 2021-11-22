13e2a2137301764cd0148c0330f08f5b44506894
Linda Zhao <linda.zhao23@gmail.com>
2021-11-22 17:06:26 -0500

    Fix P5, change outputs to integers where possible

--- Lab 8/gaussian_elimination.py
+++ Lab 8/gaussian_elimination.py
@@ -37,11 +37,10 @@ def add_rows_coefs(r1, c1, r2, c2):
     row_sum = [0] * len(r1)
 
     for i in range(len(r1)):
-        if r2[i] < 0 and c2 < 0:
-            row_sum[i] = r1[i]*c1 + abs(r2[i])*-1*c2
+        if r2[i]*c2 == int(r2[i]*c2):
+            row_sum[i] = r1[i]*c1 + int(r2[i]*c2)
         else:
-            row_sum[i] = r1[i]*c1 + abs(r2[i])*c2
-
+            row_sum[i] = r1[i]*c1 + r2[i]*c2
     return row_sum
 
 
@@ -50,24 +49,13 @@ def eliminate(M, row_to_sub, best_lead_ind): # Forward step
     '''Return M where all the rows below <row_to_sub> have the 
     values at index <best_lead_ind> eliminated. 
     Assume best_lead_ind is get_lead_ind(M[row_to_sub])'''
-    rts_lead = abs(M[row_to_sub][best_lead_ind]) # Magnitude of lead coef of row_to_sum
+    rts_lead = M[row_to_sub][best_lead_ind] # Magnitude of lead coef of row_to_sum
     rts = M[row_to_sub] # Actual row, row_to_sub
 
     for i in range(1, len(M)-row_to_sub): # Number of rows below row_to_sub
         while M[row_to_sub+i][best_lead_ind] != 0: # Number in column <best_lead_ind>
-            rts_at_bli = M[row_to_sub+i][best_lead_ind]
-            if rts_at_bli > 0: # Row below @ best_lead_ind greater than 0
-                coef = -1
-            else:
-                coef = 1
-            if rts_lead > abs(rts_at_bli): # Must subtract fraction of row_to_sub
-                M[row_to_sub+i] = add_rows_coefs(M[row_to_sub+i], 1, rts, coef*abs(rts_at_bli)/rts_lead)
-            else: # rts_lead < row below @ best_lead_ind
-                if rts_at_bli % rts_lead == 0:
-                    M[row_to_sub+i] = add_rows_coefs(M[row_to_sub+i], 1, rts, coef*abs(rts_at_bli)//rts_lead)
-                else: # rts_at_bli % rts_lead != 0:
-                    M[row_to_sub+i] = add_rows_coefs(M[row_to_sub+i], 1, rts, coef*abs(rts_at_bli)//rts_lead)
-                    M[row_to_sub+i] = add_rows_coefs(M[row_to_sub+i], 1, rts, coef*(abs(rts_at_bli)-abs(M[i][best_lead_ind]))/rts_lead)
+            row_at_bli = M[row_to_sub+i][best_lead_ind]
+            M[row_to_sub+i] = add_rows_coefs(M[row_to_sub+i], 1, rts, -1*row_at_bli/rts_lead)
     return M
 
 
