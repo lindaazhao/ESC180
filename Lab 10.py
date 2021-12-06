b32bdf183c89dfe63f952d2dd964cd03aa77606a
Linda Zhao <linda.zhao23@gmail.com>
2021-12-06 16:06:54 -0500

    More Lab 10 and Proj 3

--- Lab 10.py
+++ Lab 10.py
@@ -24,4 +24,23 @@ print(interleave([1, 3, 5, 7], [2, 4, 6, 8]))
 def reverse_rec(L):
     if len(L) == 1:
         return L
-    
+    else:
+        if L[1:len(L)-1] == []:
+            return [L[len(L)-1]] + [L[0]] # Work for even length lists too
+        else:
+            return [L[len(L)-1]] + reverse_rec(L[1:len(L)-1]) + [L[0]]
+
+print(reverse_rec([1, 2, 3, 4, 5, 6, 7]))
+
+# Problem 4
+def zigzag(L):
+    if len(L) == 0:
+        return L
+    elif len(L) % 2 == 0:
+        return [L.pop(len(L) // 2 - 1)] + zigzag(L)
+    else:
+        return [L.pop(len(L) // 2)] + zigzag(L)
+
+print(zigzag([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+# Problem 5
--- Project 3/synonyms.py
+++ Project 3/synonyms.py
@@ -5,7 +5,6 @@ def norm(vec):
     '''Return the norm of a vector stored as a dictionary,
     as described in the handout for Project 3.
     '''
-    
     sum_of_squares = 0.0  
     for x in vec:
         sum_of_squares += vec[x] * vec[x]
@@ -14,11 +13,21 @@ def norm(vec):
 
 
 def cosine_similarity(vec1, vec2):
-    pass
+    '''Return cosine similarity between sparse vectors vec1 and vec2, stored as dictionaries.'''
+    dot_prod = 0
+    for i in range(len(vec1)):
+        for j in range(len(vec2)):
+            if list(vec1.keys())[i] == list(vec2.keys())[j]:
+                dot_prod += list(vec1.values())[i] * list(vec2.values())[j]
+    
+    return dot_prod / (norm(vec1) * norm(vec2))
 
+print(cosine_similarity({"a": 1, "b": 2, "c": 3}, {"b": 4, "c": 5, "d": 6}))
 
 def build_semantic_descriptors(sentences):
-    pass
+    '''Return a dictionary d such that for every word w that appears in at least one of 
+    the sentences, d[w] is itself a dictionary which represents the semantic descriptor of w'''
+    dictionary = {}
 
 def build_semantic_descriptors_from_files(filenames):
     pass
